9f7741f299ac018a64a3bdddcdb19b121811f610
Angeline Vidhula Jeyachandra <angelinevidhulajs@gmail.com>
2015-05-13 21:10:10 +0530

    Added Component feature and changed template

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # JQuery-MVC
 An MVC architecture using JQuery for JS functions and DOM manipulations, and JSRender for templating.
+_Primarily ideated and created for my intranet dashboard project for Nike_<br/>
 
-JQuery v 1.11.2
+<h3>Dependancies</h3>
+- JQuery version 1.11.2
+- JQuery Templates
 
-JSRender beta release, currently moving towards V1.0
-
-#Demo
+<h3>Demo</h3>
 A working demo can be found <a href="http://angeline-vidhula-js.github.io/jquery-mvc/">here</a> with all latest updates.
 
-#Note
-This repo is a WIP. I created this particular MVC modularization of JQuery for a project, which I am modifying and optimizing before pushing it fully into GitHub.
+<h3>Note</h3>
+This repo is a **WIP**. I created this particular MVC modularization of JQuery for a project, which I am modifying and optimizing before pushing it fully into GitHub.
--- js/libs/jqueryMVC-1.0.js
+++ js/libs/jqueryMVC-1.0.js
@@ -18,31 +18,32 @@
         }
         return false;
     };
-	$.extend({
+    $.JQMVCComponent = {};
+    $.extend({
         Model: function(params) {
-        	var me = this,
+            var me = this,
                 $me = $(me),
-        		modelData = {
+                modelData = {
                     value: {
                         current: null,
                         existing: null
                     }
                 };
-        	// ajax fetch data
-        	me.getAjax = function (ajaxObj) {
-        		$.ajax({        			
+            // ajax fetch data
+            me.getAjax = function (ajaxObj) {
+                $.ajax({                    
                     url: ajaxObj.url,
                     async: true,
                     beforeSend: function () {
-                    	// before trigger of ajax call
-                    	// callback
-                    	if ($.isFunction(ajaxObj.beforeSend)) {
+                        // before trigger of ajax call
+                        // callback
+                        if ($.isFunction(ajaxObj.beforeSend)) {
                             ajaxObj.beforeSend();
                         }
                         $me.trigger('updateModel');
                     },
                     success: function(data, status, httpObj) {
-                    	// on success of ajax call
+                        // on success of ajax call
                         var dataObj;
                         modelData.value.existing = modelData.value.current;
                         dataObj = tryParseJSON(data);
@@ -51,28 +52,28 @@
                         } else {
                             modelData.value.current = data;
                         }
-                    	// callback
-                    	if ($.isFunction(ajaxObj.success)) {
-                            ajaxObj.success(data, status);
+                        // callback
+                        if ($.isFunction(ajaxObj.success)) {
+                            ajaxObj.success(modelData.value.current, status);
                         }
                         $me.trigger('updateModel');
                     },
                     error: function (httpObj, status, error) {
-                    	// on failure of ajax call
-                    	// callback
-                    	if ($.isFunction(ajaxObj.error)) {
+                        // on failure of ajax call
+                        // callback
+                        if ($.isFunction(ajaxObj.error)) {
                             ajaxObj.error(status);
                         }
                     },
                     complete: function () {
-                    	// completion of ajax call
-                    	// callback
-                    	if ($.isFunction(ajaxObj.complete)) {
+                        // completion of ajax call
+                        // callback
+                        if ($.isFunction(ajaxObj.complete)) {
                             ajaxObj.complete();
                         }
                     }
-        		});
-        	};
+                });
+            };
             // get model data
             me.getData = function () {
                 return modelData.value.current;
@@ -95,7 +96,8 @@
         View: function (params) {
             var me = this,
                 view,
-                template;
+                template,
+                rendered;
             // to fetch the model corresponing to the view
             me.getModel = function () {
                 return params.model;
@@ -127,7 +129,8 @@
             view = {
                 // init function
                 _initialize: function() {
-                    // this._fetchTemplate({});
+                    rendered = $('<div class="jquerymvc-tmpl" id="jquerymvc-tmpl-' + $('.jquerymvc-tmpl').length + '"></div>');
+                    rendered.appendTo(params.el);
                 },
                 // fetch the template if not fetched
                 _fetchTemplate: function (data) {
@@ -135,12 +138,20 @@
                         url: params.templateUrl,
                         async: true,
                         success: function(htmlTpl, status, httpObj) {
-                            var html;
-                            template = $.templates("tpl", htmlTpl);
-                            html = template.render(data);
-                            params.el.html(html);
-                            // var tmplEncode = $.templates(htmlTpl, data).appendTo(params.el);
-                            // template = $.tmplItem(tmplEncode);
+                            var html,
+                                components,
+                                componentEl,
+                                lp = 0;
+                            template = $.template(null, htmlTpl);
+                            html = $.tmpl(template, data)
+                            rendered.html(html);
+                            // component initialization
+                            components = rendered.find('[data-component]');
+                            // initialize the component
+                            for(lp ; lp < components.length ; lp++) {
+                                componentEl = $(components[lp]);
+                                new $.Component(componentEl.attr('data-component'), componentEl);
+                            }
                         }
                     });
                 },
@@ -148,8 +159,18 @@
                 _render: function(data) {
                     if(template) {
                         // update the template with new data
-                        var html = template.render(data);
-                        params.el.html(html);
+                        var html = $.tmpl(template, data)
+                        rendered.html(html); 
+                        var components,
+                            componentEl,
+                            lp = 0;
+                        // component initialization
+                        components = rendered.find('[data-component]');
+                        // initialize the component
+                        for(lp ; lp < components.length ; lp++) {
+                            componentEl = $(components[lp]);
+                            new $.Component(componentEl.attr('data-component'), componentEl);
+                        }
                     } else {
                         this._fetchTemplate(data);
                     }
@@ -163,7 +184,7 @@
                 model = params.view.getModel(),
                 controller,
                 el = params.view.getEl(),
-                events = [];
+                events = {};
             // to fetch the view corresponding to the controller
             me.getView = function () {
                 return params.view;
@@ -179,6 +200,15 @@
             // fetch the element
             me.getEl = function () {
                 return el;
+            };
+            // fetch the evtnts
+            me.getEvents = function () {
+                return events;
+            };
+            // remove events
+            me.offEvent = function (evt, selector) {
+                el.off(evt, selector);
+                delete events[evt][selector];
             };
 
             // controller internal functions
@@ -199,8 +229,17 @@
 
             controller._initialize();
         },
-        Component: function (params) {
+        Component: function (componentname, element) {
+            var component = {};
 
+            // private functions
+            component = {
+                _initialize: function () {
+                    $.JQMVCComponent[componentname].apply(this, element);
+                }
+            };
+
+            component._initialize();
         }
     });
 })(window.jQuery);
